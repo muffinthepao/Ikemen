72cd02274fb5c4de350487140b48906567fb4e91
Mervin Poh <mervpoh@gmail.com>
2022-07-29 22:49:31 +0800

    display api data on /food

--- controllers/pages/page_controller.js
+++ controllers/pages/page_controller.js
@@ -1,25 +1,24 @@
 const controller = {
-
     showHome: (req, res) => {
-        res.render('./pages/home.ejs')
+        res.render("./pages/home.ejs");
     },
 
     showFood: async (req, res) => {
         // res.send('listings')
-        const url = 'https://api.yelp.com/v3/businesses/search?term=food&location=Singapore'
+        const url ="https://api.yelp.com/v3/businesses/search?term=food&location=Singapore&limit=20&offset=40";
         const response = await fetch(url, {
             method: "GET",
             headers: {
-                Authorization: `Bearer ${process.env.YELP_API}`
-            }
+                Authorization: process.env.YELP_API,
+            },
         });
 
-        const json = await response.json()
+        const json = await response.json();
+        const foodListings = json.businesses
 
-        res.send(json)
+        // res.send(foodListings[0]);
+        res.render("./pages/food.ejs", {foodListings})
     },
+};
 
-
-}
-
-module.exports = controller
+module.exports = controller;
