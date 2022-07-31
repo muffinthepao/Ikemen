269c20d068c4cca18b5e0345971d94c926d0f901
Mervin Poh <mervpoh@gmail.com>
2022-07-31 18:32:21 +0800

    yelp review still now stars

--- controllers/pages/page_controller.js
+++ controllers/pages/page_controller.js
@@ -1,54 +1,68 @@
 // const food = require('../../source/yelp_api')
 
+const YELP_API_URI = "https://api.yelp.com/v3/businesses";
+
 const controller = {
-  showHome: (req, res) => {
-      res.render("./pages/home.ejs");
-  },
+    showHome: (req, res) => {
+        res.render("./pages/home.ejs");
+    },
 
-  showListings: async (req, res) => {
-      try {
-          const url ="https://api.yelp.com/v3/businesses/search?term=noodles&location=Singapore&limit=20";
-          const response = await fetch(url, {
-              method: "GET",
-              headers: {
-                  Authorization: process.env.YELP_API,
-              },
-          });
+    showListings: async (req, res) => {
+        try {
+            const url =
+                "https://api.yelp.com/v3/businesses/search?term=noodles&location=Singapore&limit=20";
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    Authorization: process.env.YELP_API,
+                },
+            });
 
-          const json = await response.json();
-          const foodListings = json.businesses;
+            const json = await response.json();
+            const foodListings = json.businesses;
 
-          // res.send(foodListings[0]);
-          res.render("./pages/food.ejs", { foodListings });
-      } catch (err) {
-          console.log(err);
-          res.send("Failed to fetch API");
-      }
-      // res.send(foodListings[0]);
-  },
+            // res.send(foodListings[0]);
+            res.render("./pages/food.ejs", { foodListings });
+        } catch (err) {
+            console.log(err);
+            res.send("Failed to fetch API");
+        }
+        // res.send(foodListings[0]);
+    },
 
-  showIndividualListing: async (req, res) => {
-      // res.render("./pages/home.ejs");
-      const listingID = req.params.listing_id
+    showIndividualListing: async (req, res) => {
+        // res.render("./pages/home.ejs");
+        const listingID = req.params.listing_id;
 
-      try {
-        const url =`https://api.yelp.com/v3/businesses/${listingID}`;
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                Authorization: process.env.YELP_API,
-            },
-        });
+        try {
+            const listingCall = `${YELP_API_URI}/${listingID}`;
+            const listingResponse = await fetch(listingCall, {
+                method: "GET",
+                headers: {
+                    Authorization: process.env.YELP_API,
+                },
+            });
 
-        const listing = await response.json();
-        res.render("./pages/listing.ejs", { listing });
-        // res.render('./pages/listing.ejs', {listingID})
-        
-      } catch (err) {
-        console.log(err)
-      }
+            const listingReviewsCall = `${YELP_API_URI}/${listingID}/reviews`;
+            const listingReviewsResponse = await fetch(listingReviewsCall, {
+                method: "GET",
+                headers: {
+                    Authorization: process.env.YELP_API,
+                },
+            });
 
-  },
+            const listing = await listingResponse.json();
+            const listingLocation = listing.location;
+            
+            const listingReviews = await listingReviewsResponse.json();
+            const allListingReviews = listingReviews.reviews;
+
+            // console.log(listing)
+            res.render("./pages/listing.ejs", { listing, listingLocation, allListingReviews });
+        } catch (err) {
+            console.log(err);
+        }
+    },
 };
 
 module.exports = controller;
--- public/map.js
+++ public/map.js
@@ -1,57 +1,57 @@
-"use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
+// "use strict";
+// // Object.defineProperty(exports, "__esModule", { value: true });
 
-// Initialize and add the map
-function initMap() {
-    const singaporeLatLong = { lat: 1.36548045615708, lng: 103.82020348793333 };
-    // The map, centered at Singapore
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: singaporeLatLong,
-        zoom: 12,
-    });
-    // The marker, positioned at Singapore
-    // const marker = new google.maps.Marker({
-    //     position: singaporeLatLong,
-    //     map: map,
-    // });
+// // Initialize and add the map
+// function initMap() {
+//     const singaporeLatLong = { lat: 1.36548045615708, lng: 103.82020348793333 };
+//     // The map, centered at Singapore
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         center: singaporeLatLong,
+//         zoom: 12,
+//     });
+//     // The marker, positioned at Singapore
+//     // const marker = new google.maps.Marker({
+//     //     position: singaporeLatLong,
+//     //     map: map,
+//     // });
 
-    //create the places service
-    let service = new google.maps.places.PlacesService(map);
+//     //create the places service
+//     let service = new google.maps.places.PlacesService(map);
 
-    let request = {
-        query: "Bak Chor Mee",
-        fields: ["name", "geometry"],
-    };
+//     let request = {
+//         query: "Bak Chor Mee",
+//         fields: ["name", "geometry"],
+//     };
 
-    console.log(request)
+//     console.log(request)
 
-    // The marker, positioned at Singapore
-    // const marker = new google.maps.Marker({
-    //     position: request,
-    //     map: map,
-    // });
+//     // The marker, positioned at Singapore
+//     // const marker = new google.maps.Marker({
+//     //     position: request,
+//     //     map: map,
+//     // });
 
-    function createMarker(place) {
-        if (!place.geometry || !place.geometry.location) return;
-        let marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location,
-        });
-        google.maps.event.addListener(marker, "click", function () {
-            infowindow.setContent(place.name || "");
-            infowindow.open(map);
-        });
-    }
+//     function createMarker(place) {
+//         if (!place.geometry || !place.geometry.location) return;
+//         let marker = new google.maps.Marker({
+//             map: map,
+//             position: place.geometry.location,
+//         });
+//         google.maps.event.addListener(marker, "click", function () {
+//             infowindow.setContent(place.name || "");
+//             infowindow.open(map);
+//         });
+//     }
 
-    service.textSearch(request, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (let i = 0; i < results.length; i++) {
-                console.log(results[i]);
-                createMarker(results[i]);
-            }
-            map.setCenter(results.geometry.location);
-        }
-    });
-}
+//     service.textSearch(request, function (results, status) {
+//         if (status === google.maps.places.PlacesServiceStatus.OK) {
+//             for (let i = 0; i < results.length; i++) {
+//                 console.log(results[i]);
+//                 createMarker(results[i]);
+//             }
+//             map.setCenter(results.geometry.location);
+//         }
+//     });
+// }
 
-window.initMap = initMap;
+// window.initMap = initMap;
