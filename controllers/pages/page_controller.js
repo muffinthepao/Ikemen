a178393fb0eddffcbaeda570189c55e7e075ff1b
Mervin Poh <mervpoh@gmail.com>
2022-08-10 23:38:31 +0800

    pagination on /food is done

--- controllers/pages/page_controller.js
+++ controllers/pages/page_controller.js
@@ -5,54 +5,92 @@ const yelpAPIBase = "https://api.yelp.com/v3/businesses";
 
 const listingModel = require("../../models/listings/listings");
 const reviewModel = require("../../models/reviews/reviews");
-const userModel = require('../../models/authentication/users')
+const userModel = require("../../models/authentication/users");
 
 const controller = {
-    showHome: (req, res) => {
-        res.locals.firstName = "hello"
-        res.locals.lastName = "world"
-        res.render("./pages/home.ejs");
-    },
+  showHome: (req, res) => {
+    res.locals.firstName = "hello";
+    res.locals.lastName = "world";
+    res.render("./pages/home.ejs");
+  },
 
-    showListings: async (req, res) => {
-        let errorObject = {}
+  showListings: async (req, res) => {
+    let errorObject = {};
 
-        const listOfFood = await yelpAPI(allListingsURI);
-        const foodListings = listOfFood.businesses
-        res.render("./pages/food.ejs", { foodListings, errorObject });
-    },
+    const page = parseInt(req.query.page)
+    let pageArray = []
+    const limit = 20
+    const offset = (page - 1) * limit
 
-    showIndividualListing: async (req, res) => {
-        let errorObject = {}
+    if (page < 4) {
+      pageArray = [1, 2, 3, 4, 5]
+    } else {
+      pageArray = [page - 2, page - 1, page, page + 1, page + 2]
+    }
 
-        const listingID = req.params.listing_id;
-        const listingCall = `${yelpAPIBase}/${listingID}`;
+    console.log(offset)
+    
+    if(page) {
+      const updatedAllListingsURI =`https://api.yelp.com/v3/businesses/search?term=noodles&location=Singapore&limit=20&offset=${offset}`;
 
-        //Individual Listing
-        const listing = await yelpAPI(listingCall);
-        const listingLocation = listing.location;
+      const listOfFood = await yelpAPI(updatedAllListingsURI);
+      const foodListings = listOfFood.businesses;
+      res.render("./pages/food.ejs", { foodListings, errorObject, page, pageArray });
+      return
+    } 
 
-        //Yelp Reviews. Max of 3 reviews. Yelp API limitation. 
-        const yelpReviews = await yelpAPI(`${listingCall}/reviews`)
-        const allYelpReviews = yelpReviews.reviews
+    if(page > 50 || page < 0) {
+      const listOfFood = await yelpAPI(allListingsURI);
+      const foodListings = listOfFood.businesses;
+    res.redirect('/food', {foodListings})
 
-        //Ikemen Reviews
-        const allIkemenReviews = await reviewModel.find({yelpID: listingID}).populate([{
-            path: "user",
-            select: "fullName preferredName email"
-        }])
+    }
 
-        console.log(allIkemenReviews)
 
-        res.render("./pages/listing.ejs", {
-          listing,
-          listingID,
-          listingLocation,
-          allIkemenReviews,
-          allYelpReviews,
-          errorObject
-        });
-    },
+    const listOfFood = await yelpAPI(allListingsURI);
+    const foodListings = listOfFood.businesses;
+    res.render("./pages/food.ejs", { foodListings, errorObject, page });
+  },
+
+  showIndividualListing: async (req, res) => {
+    let errorObject = {};
+
+    const listingID = req.params.listing_id;
+    const listingCall = `${yelpAPIBase}/${listingID}`;
+
+    //Individual Listing
+    const listing = await yelpAPI(listingCall);
+    const listingLocation = listing.location;
+
+    //Yelp Reviews. Max of 3 reviews. Yelp API limitation.
+    const yelpReviews = await yelpAPI(`${listingCall}/reviews`);
+    const allYelpReviews = yelpReviews.reviews;
+
+    //Ikemen Reviews
+    const allIkemenReviews = await reviewModel
+      .find({ yelpID: listingID })
+      .populate([
+        {
+          path: "user",
+          select: "fullName preferredName email",
+        },
+        // {
+        //   path: "reviews",
+          
+        // },
+      ]);
+
+    console.log(allIkemenReviews);
+
+    res.render("./pages/listing.ejs", {
+      listing,
+      listingID,
+      listingLocation,
+      allIkemenReviews,
+      allYelpReviews,
+      errorObject,
+    });
+  },
 };
 
 module.exports = controller;
--- controllers/reviews/review_controller.js
+++ controllers/reviews/review_controller.js
@@ -171,7 +171,7 @@ const controller = {
 
     } catch (err) {
       console.log(err)
-      res.send("cannot edit review right now")
+      res.send("cannot edit review right now")``
     }
   },
 
@@ -179,25 +179,22 @@ const controller = {
     const listingID = req.params.listing_id;
     const reviewID = req.params.review_id;
 
-    console.log(reviewID)
-
-    // try {
-    //   await reviewModel.findByIdAndDelete(reviewID);
-    //   await listingModel.findOneAndUpdate({yelpID: listingID}, {
-    //       $pull: {
-    //           reviews: reviewID
-    //       },
-    //       $inc: {
-    //         reviewCount: -1
-    //       },
-    //   });
-    //   res.redirect(`/food/${listingID}`);
+    try {
+      await reviewModel.findByIdAndDelete(reviewID);
+      await listingModel.findOneAndUpdate({yelpID: listingID}, {
+          $pull: {
+              reviews: reviewID
+          },
+          $inc: {
+            reviewCount: -1
+          },
+      });
 
 
-    // } catch (err) {
-    //   console.log(err)
-    //   res.send("cannot delete review at the moment. please try again later")
-    // }
+    } catch (err) {
+      console.log(err)
+      res.send("cannot delete review at the moment. please try again later")
+    }
   }
 };
 
